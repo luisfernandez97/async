148748d7f55d5a26741460661be2bdd8826c8d9b
LuisFernandez <luiselias.fernandez97@gmail.com>
2022-04-16 21:56:46 -0400

    Promises updated

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -4,20 +4,15 @@ const fetchData = (url_api) => {
   return new Promise((resolve, reject) => {
     const xhttp = new XMLHttpRequest();
     xhttp.open("GET", url_api, true);
-    xhttp.onreadystatechange = function (event) {
+    xhttp.onreadystatechange = () => {
       if (xhttp.readyState === 4) {
-        if(xhttp.status === 200)
-          ? resolve (JSON.parse(xhttp.responseText))
-          : reject 
-
-        if (xhttp.status === 200) {
-          callback(null, JSON.parse(xhttp.responseText));
-        } else {
-          const error = new Error("Error " + url_api);
-          return callback(error, null);
-        }
+        xhttp.status === 200
+          ? resolve(JSON.parse(xhttp.responseText))
+          : reject(new Error("Error ", url_api));
       }
     };
     xhttp.send();
   });
 };
+
+module.exports = fetchData;
